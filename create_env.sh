d11a3d37f3c455df653f943d86cc25b6b8a11176
Sam Hardy <samhardyhey@gmail.com>
2022-09-26 08:25:56 +1000

    add runpod creat env script

---
+++ create_env.sh
@@ -0,0 +1,20 @@
+conda config --set auto_activate_base false
+
+echo "Creating p38 conda env"
+conda create -n p38 python=3.8 -y
+eval "$(conda shell.bash hook)"
+conda activate p38
+
+echo "Installing project requirements"
+pip install -r ./requirements.txt
+
+echo "Installing torch/conda binaries"
+conda remove pytorch torchvision torchaudio -y # silly jarvis env
+conda install pytorch torchvision torchaudio -c pytorch -y
+
+echo "Testing torch installation"
+python -c 'import torch; print(torch.cuda.is_available()); print(torch.cuda.device_count())'
+
+echo "Add git config"
+git config --global user.name "Sam Hardy"
+git config --global user.email "samhardyhey@gmail.com"
